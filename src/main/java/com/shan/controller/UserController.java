d0d1d44ef5f1e557becdb8803305f2ce69162134
shancong <cong.shan@chinaventure.com.cn>
2018-09-02 14:20:53 +0800

    idea第一次提交

--- src/main/java/com/shan/controller/UserController.java
+++ src/main/java/com/shan/controller/UserController.java
@@ -16,6 +16,12 @@ public class UserController {
 		map.put("id", 1);
 		map.put("name", "张三");
 		System.out.println("e2提交");
+		System.out.println("idea提交第一次");
 		return map;
 	}
+	
+	 public static void main(String[] args){
+	  System.out.println("idea");
+	}
+	
 }
