f23278b77dd67da69b2440b6b35b2c5a0558a2d4
shancong <cong.shan@chinaventure.com.cn>
2018-09-02 16:19:06 +0800

    idea第2次提交 会有冲突

---
+++ src/main/java/com/shan/controller/Test.java
@@ -0,0 +1,15 @@
+package com.shan.controller;
+
+/**
+ * @author SC
+ * @date 2018/9/2 16:17
+ * @Company：zhidain
+ * @description:
+ */
+public class Test {
+    
+    public static void main(String[] args){
+        System.out.println("创建dev分支");
+    }
+    
+}
