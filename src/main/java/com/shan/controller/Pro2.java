7666352459c8ed318bcfc8134168fdc41b898e51
shancong <cong.shan@chinaventure.com.cn>
2018-10-05 15:55:49 +0800

    idea2在pro2分支+1

---
+++ src/main/java/com/shan/controller/Pro2.java
@@ -0,0 +1,14 @@
+package com.shan.controller;
+
+/**
+ * @author ShanCong
+ * @date 2018/10/5 15:55
+ * @description:
+ */
+public class Pro2 {
+
+
+    public static void main(String[] args){
+        System.out.println("pro2分支");
+    }
+}
